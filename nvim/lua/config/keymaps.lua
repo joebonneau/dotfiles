510f139deb8bfca9b8c4567bdf4252e140186065
Joe Bonneau <bonneau@amyris.com>
2023-10-13 09:29:34 -0700

    fix git diff finder keymap to use entry_maker instead of directly editing file names

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,9 +4,10 @@
 vim.keymap.set("i", "<C-e>", "<ESC>", { desc = "Exit insert mode with C-e" })
 vim.keymap.set("n", "<leader>gm", "<Cmd>Gvdiffsplit!<CR>", { silent = true, desc = "3-pane resolve merge conflict" })
 
-function _G.get_git_diff_files(opts)
+function _G.get_git_diff_files()
   local pickers = require("telescope.pickers")
   local finders = require("telescope.finders")
+  local previewers = require("telescope.previewers")
   local conf = require("telescope.config").values
   local list = vim.fn.systemlist("git diff --name-only $(git merge-base $(git branch --show-current) HEAD)")
   local icons = require("nvim-web-devicons")
@@ -16,17 +17,36 @@ function _G.get_git_diff_files(opts)
     if string.find(text, "Not a git repository") then
       is_git_repo = false
       break
-    else
-      local icon, _ = icons.get_icon(text, vim.fn.fnamemodify(text, ":e"), { default = true })
-      table.insert(list_with_icons, icon .. " " .. text)
+      -- else
+      --   local icon, _ = icons.get_icon(text, vim.fn.fnamemodify(text, ":e"), { default = true })
+      --   table.insert(list_with_icons, icon .. " " .. text)
     end
   end
   if is_git_repo then
+    local opts = {
+      layout_strategy = "horizontal",
+      layout_config = {
+        width = 0.5,
+        height = 0.5,
+      },
+    }
     pickers
       .new(opts, {
         prompt_title = "Find in Git Diff",
-        finder = finders.new_table({ results = list_with_icons }),
-        sorter = conf.generic_sorter(opts),
+        finder = finders.new_table({
+          results = list,
+          entry_maker = function(entry)
+            local icon, _ = icons.get_icon(entry.value, vim.fn.fnamemodify(entry, ":e"), { default = true })
+            return {
+              value = entry,
+              ordinal = entry,
+              display = icon .. " " .. entry,
+            }
+          end,
+        }),
+
+        -- finder = finders.new_table({ results = list_with_icons }),
+        sorter = conf.file_sorter(opts),
       })
       :find()
   else
