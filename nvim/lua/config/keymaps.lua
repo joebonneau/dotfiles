c79885936deab2aa2ce019589c279cb27139bcc6
Joe Bonneau <bonneau@amyris.com>
2023-10-12 20:04:13 -0700

    add keymaps for searching through git diff files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,16 +25,57 @@ function _G.git_diff(opts)
   end
   if is_git_repo then
     pickers
-        .new(opts, {
-          prompt_title = "files present in git diff",
-          finder = finders.new_table({ results = list }),
-          sorter = conf.generic_sorter(opts),
-        })
-        :find()
+      .new(opts, {
+        prompt_title = "files present in git diff",
+        finder = finders.new_table({ results = list }),
+        sorter = conf.generic_sorter(opts),
+      })
+      :find()
   else
     local notify = require("notify")
     notify("No files present in git diff.", vim.log.levels.WARN, { title = "Find in git diff" })
   end
 end
 
-vim.keymap.set("n", "<leader>fd", "<:lua git_diff()<CR>", { desc = "Find in git diff" })
+function _G.git_diff_select_branch(opts)
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  local compare_to_branch = "master"
+  local branches = vim.fn.systemlist("git branch --all")
+  vim.ui.select(branches, {}, function(input)
+    if input ~= "" then
+      compare_to_branch = input
+      -- try to get diff from master, if not, try from main
+      local list = vim.fn.systemlist(string.format("git diff --name-only %s", compare_to_branch))
+      for _, text in ipairs(list) do
+        if string.find(text, "fatal") then
+          list = vim.fn.systemlist("git diff --name-only main")
+          break
+        end
+      end
+      local is_git_repo = true
+      for _, text in ipairs(list) do
+        if string.find(text, "Not a git repository") then
+          is_git_repo = false
+          break
+        end
+      end
+      if is_git_repo then
+        pickers
+          .new(opts, {
+            prompt_title = "files present in git diff",
+            finder = finders.new_table({ results = list }),
+            sorter = conf.generic_sorter(opts),
+          })
+          :find()
+      else
+        local notify = require("notify")
+        notify("No files present in git diff.", vim.log.levels.WARN, { title = "Find in git diff" })
+      end
+    end
+  end)
+end
+
+vim.keymap.set("n", "<leader>fD", "<:lua git_diff_select_branch()<CR>", { desc = "Find in git diff (select)" })
+vim.keymap.set("n", "<leader>fd", "<:lua git_diff()<CR>", { desc = "Find in git diff (master)" })
