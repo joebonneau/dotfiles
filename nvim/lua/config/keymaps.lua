faccd6928b5c4066f1f818de8641c21fc6d69ac4
Joe Bonneau <bonneau@amyris.com>
2023-10-12 21:36:43 -0700

    add icons to results

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,24 +9,29 @@ function _G.get_git_diff_files(opts)
   local finders = require("telescope.finders")
   local conf = require("telescope.config").values
   local list = vim.fn.systemlist("git diff --name-only $(git merge-base $(git branch --show-current) HEAD)")
+  local icons = require("nvim-web-devicons")
   local is_git_repo = true
+  local list_with_icons = {}
   for _, text in ipairs(list) do
     if string.find(text, "Not a git repository") then
       is_git_repo = false
       break
+    else
+      local icon, _ = icons.get_icon(text, vim.fn.fnamemodify(text, ":e"), { default = true })
+      table.insert(list_with_icons, icon .. " " .. text)
     end
   end
   if is_git_repo then
     pickers
       .new(opts, {
-        prompt_title = "files present in git diff",
-        finder = finders.new_table({ results = list }),
+        prompt_title = "Find in Git Diff",
+        finder = finders.new_table({ results = list_with_icons }),
         sorter = conf.generic_sorter(opts),
       })
       :find()
   else
     local notify = require("notify")
-    notify("No files present in git diff.", vim.log.levels.WARN, { title = "Find in git diff" })
+    notify("Current directory is not a git repo.", vim.log.levels.WARN, { title = "Find in git diff" })
   end
 end
 
