acc332074aaceda0169bb0d46b671b3c1f9b23c0
Joe Bonneau <bonneau@amyris.com>
2023-10-15 16:21:42 -0700

    add new lualine.nvim config

---
+++ nvim/lua/plugins/lualine-nvim.lua
@@ -0,0 +1,92 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function()
+    local icons = require("lazyvim.config").icons
+    local Util = require("lazyvim.util")
+    local colors = {
+      [""] = Util.ui.fg("Special"),
+      ["Normal"] = Util.ui.fg("Special"),
+      ["Warning"] = Util.ui.fg("DiagnosticError"),
+      ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
+    }
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          {
+            "branch",
+            fmt = function(branch)
+              return string.len(branch) > 30 and string.sub(branch, 1, 30) .. "..." or branch
+            end,
+          },
+        },
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", path = 0, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          {
+            function()
+              return Util.root.pretty_path()
+            end,
+          },
+        },
+        lualine_x = {
+          {
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.command.has()
+            end,
+            color = Util.ui.fg("Statement"),
+          },
+          {
+            function()
+              local icon = require("lazyvim.config").icons.kinds.Copilot
+              local status = require("copilot.api").status.data
+              return icon .. (status.message or "")
+            end,
+            cond = function()
+              if not package.loaded["copilot"] then
+                return
+              end
+              local ok, clients = pcall(require("lazyvim.util").lsp.get_clients, { name = "copilot", bufnr = 0 })
+              if not ok then
+                return false
+              end
+              return ok and #clients > 0
+            end,
+            color = function()
+              if not package.loaded["copilot"] then
+                return
+              end
+              local status = require("copilot.api").status.data
+              return colors[status.status] or colors[""]
+            end,
+          },
+        },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {},
+      },
+      extensions = { "neo-tree", "lazy" },
+    }
+  end,
+}
