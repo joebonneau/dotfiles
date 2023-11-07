51399112d4c5896c0afaad82a5fb9664b36256c7
Joe Bonneau <bonneau@amyris.com>
2023-11-06 16:56:54 -0800

    update ts script

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -25,3 +25,6 @@ abbr adr "devscripts adr"
 abbr dev-devscripts "docker run --volume=$pwd:/app --user "(id -u):(id -g)" -it --rm dev-scripts_app:latest"
 abbr dev-aformat "dev-devscripts aformat"
 abbr dev-adr "dev-devscripts adr"
+
+# aliases for ease of use in neovim (abbr don't expand)
+alias pydoc "python3 -m pydoc"
