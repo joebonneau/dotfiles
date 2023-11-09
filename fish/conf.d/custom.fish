d66ea70ae56239123a9815c5c02f51340f6dc4bc
Joe Bonneau <joebonneau@gmail.com>
2023-11-08 20:38:13 -0800

    wip

--- fish/conf.d/custom.fish
+++ fish/conf.d/custom.fish
@@ -26,21 +26,21 @@ function docker-refresh-openapi
     end
 end
 
-function symlink --argument _from _to
-    if test "$_from" = ""; or test "$_to" = ""
-        echo "symlink: must provide from and to arguments"
-        return 1
-    end
-
-    set abs_to (realpath "$_to")
-
-    if test -d $_to && not test -d $_from
-        set to "$abs_to/"(basename $_from)
-    else
-        set to "$abs_to"
-    end
-
-    set from (realpath "$_from")
-
-    ln -s $from $to
-end
+# function symlink --argument _from _to
+#     if test "$_from" = ""; or test "$_to" = ""
+#         echo "symlink: must provide from and to arguments"
+#         return 1
+#     end
+#
+#     set abs_to (realpath "$_to")
+#
+#     if test -d $_to && not test -d $_from
+#         set to "$abs_to/"(basename $_from)
+#     else
+#         set to "$abs_to"
+#     end
+#
+#     set from (realpath "$_from")
+#
+#     ln -s $from $to
+# end
