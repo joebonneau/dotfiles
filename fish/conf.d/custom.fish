07e5d29cbd93ac0f40517c216ccb461f74f666e5
Joe Bonneau <bonneau@amyris.com>
2023-11-09 16:20:19 -0800

    move docker-refresh-openapi function to a script; cleanup

--- fish/conf.d/custom.fish
+++ fish/conf.d/custom.fish
@@ -1,30 +1,30 @@
 
-function docker-refresh-openapi
-    set webhash (docker ps | grep -m 1 "hermes_web" | awk '{print $1}')
-    set webname (docker ps | grep -m 1 "hermes_web" | awk '{print $2}')
-    set fehash (docker ps | grep -m 1 "hermes_frontend" | awk '{print $1}')
-    set fename (docker ps | grep -m 1 "hermes_frontend" | awk '{print $2}')
-    # read webhash webname <<< (docker ps | grep -m 1 "hermes_web" | awk '{print $1 " " $2}')
-    # read fehash fename <<< (docker ps | grep -m 1 "hermes_frontend" | awk '{print $1 " " $2}')
-
-    if [ -z "$webhash" ] && [ -z "$fehash" ]
-        echo "*** The Hermes web and/or frontend container is not running!"
-    else
-        echo "===== Copying $1 to $fename ====="
-        docker cp $1 $fehash:/app/hermes/public/__openapi__
-
-        echo "===== Rebuilding the openapi.yml file and copying... ====="
-        docker exec $fehash /bin/bash -c "npx @redocly/cli bundle /app/hermes/public/__openapi__/openapi.yml --output /app/hermes/public/openapi.yml --ext yml"
-
-        echo "===== Copying the combined file to the $webname container... ====="
-        docker cp $fehash:/app/hermes/public/openapi.yml combined_openapi.yml
-        docker cp combined_openapi.yml $webhash:/app/hermes/public/openapi.yml
-        rm combined_openapi.yml
-
-        echo -e "===== Success! Restarting containers... ====="
-        docker-compose restart web frontend app
-    end
-end
+# function docker-refresh-openapi
+#     set webhash (docker ps | grep -m 1 "hermes_web" | awk '{print $1}')
+#     set webname (docker ps | grep -m 1 "hermes_web" | awk '{print $2}')
+#     set fehash (docker ps | grep -m 1 "hermes_frontend" | awk '{print $1}')
+#     set fename (docker ps | grep -m 1 "hermes_frontend" | awk '{print $2}')
+#     # read webhash webname <<< (docker ps | grep -m 1 "hermes_web" | awk '{print $1 " " $2}')
+#     # read fehash fename <<< (docker ps | grep -m 1 "hermes_frontend" | awk '{print $1 " " $2}')
+#
+#     if [ -z "$webhash" ] && [ -z "$fehash" ]
+#         echo "*** The Hermes web and/or frontend container is not running!"
+#     else
+#         echo "===== Copying $1 to $fename ====="
+#         docker cp "$1" "$fehash":/app/hermes/public/__openapi__
+#
+#         echo "===== Rebuilding the openapi.yml file and copying... ====="
+#         docker exec "$fehash" /bin/bash -c "npx @redocly/cli bundle /app/hermes/public/__openapi__/openapi.yml --output /app/hermes/public/openapi.yml --ext yml"
+#
+#         echo "===== Copying the combined file to the $webname container... ====="
+#         docker cp "$fehash":/app/hermes/public/openapi.yml combined_openapi.yml
+#         docker cp combined_openapi.yml "$webhash":/app/hermes/public/openapi.yml
+#         rm combined_openapi.yml
+#
+#         echo -e "===== Success! Restarting containers... ====="
+#         docker-compose restart web frontend app
+#     end
+# end
 
 # function symlink --argument _from _to
 #     if test "$_from" = ""; or test "$_to" = ""
