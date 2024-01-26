ba52f7f1afd8549577cc7c774871bd036593a9f0
Joe Bonneau <joebonneau@gmail.com>
2024-01-25 21:49:59 -0800

    relocate sesh scripts and update config

--- bin/sesh/sesh.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-tmux new-window
-tmux send-keys "echo hello" Enter
-session_name=$(tmux display-message -p "#S")
-# tmux switch-client -t "$session_name:nvim"
---
+++ sesh/bin/spotify.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+spotify_player
