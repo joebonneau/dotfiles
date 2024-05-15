d840cd95f8ee4677d37dc0ff10c0cac9ab511464
Joe Bonneau <joebonneau@gmail.com>
2024-05-14 20:54:09 -0700

    nvim: remove some unused plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "catppuccin/nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/neoconf.nvim", enabled = false },
 }
