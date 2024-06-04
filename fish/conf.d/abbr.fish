3e661c85b6c9ba4107b3c063313197520c53b582
Joe Bonneau <joebonneau@gmail.com>
2024-06-03 21:15:56 -0700

    fish: remove old work scripts

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -2,6 +2,7 @@
 abbr g git
 abbr gst "git status"
 abbr gc "git commit -m"
+abbr gr "git rebase"
 abbr ga "git add"
 abbr gp "git push"
 abbr gcb "git checkout -b"
@@ -31,16 +32,6 @@ abbr hfzf "history | fzf"
 abbr c clear
 abbr ef "source ~/git_repos/dotfiles/fish/conf.d/abbr.fish && exec fish"
 abbr efa exec-all-sh
-
-# dev-scripts
-abbr update-devscripts "docker pull git.amyris.local:4567/amyris/dev-scripts:latest"
-abbr devscripts "docker run --volume=$pwd:/app -e GIT_DISCOVERY_ACROSS_FILESYSTEM=1 -it --rm git.amyris.local:4567/amyris/dev-scripts:latest"
-abbr metadata "devscripts metadata"
-abbr aformat "devscripts aformat"
-abbr adr "devscripts adr"
-abbr dev-devscripts "docker run --volume=$pwd:/app --user "(id -u):(id -g)" -it --rm dev-scripts_app:latest"
-abbr dev-aformat "dev-devscripts aformat"
-abbr dev-adr "dev-devscripts adr"
 
 # aliases for ease of use in neovim (abbr don't expand)
 alias pydoc "python3 -m pydoc"
