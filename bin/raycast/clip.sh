f42b7f685823aa6290b607d0b86f07c7fb419d56
Joe Bonneau <joebonneau@gmail.com>
2024-06-13 15:50:52 -0700

    feat: add raycast scripts to quickly access clipboard and snippet search

---
+++ bin/raycast/clip.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title clip
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📋
+
+# Documentation:
+# @raycast.description Quickly open Clipboard History
+# @raycast.author joe_bonneau
+# @raycast.authorURL https://raycast.com/joe_bonneau
+
+open raycast://extensions/raycast/clipboard-history/clipboard-history
---
+++ bin/raycast/snip.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title snip
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ✂️
+
+# Documentation:
+# @raycast.description Quickly open Snippets
+# @raycast.author joe_bonneau
+# @raycast.authorURL https://raycast.com/joe_bonneau
+
+open raycast://extensions/raycast/snippets/search-snippets
