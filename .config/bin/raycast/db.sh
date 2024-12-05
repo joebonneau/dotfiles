a43153d89245733d30f2a5ce5e19a175e8195b4d
Joe Bonneau <joebonneau@gmail.com>
2024-12-04 21:09:52 -0800

    fix(raycast): properly switch to db session or previous workspace

--- .config/bin/raycast/db.sh
+++ .config/bin/raycast/db.sh
@@ -14,7 +14,17 @@
 # @raycast.author joe_bonneau
 # @raycast.authorURL https://raycast.com/joe_bonneau
 
-# FIXME: if already in Alacritty but not in the right session, switch to the session
-# instead of switching workspaces
-aerospace workspace --auto-back-and-forth A
-sesh connect --switch db
+aerospace workspace --fail-if-noop A 2>/dev/null
+# check if already on terminal workspace
+if [[ $? == 1 ]]; then
+  active_session="$(sesh list | head -n 1)"
+  if [[ "$active_session" != "db" ]]; then
+    # switch to the db session if not currently on it
+    sesh connect --switch db
+    exit 0
+  else
+    aerospace workspace-back-and-forth
+  fi
+else
+  sesh connect --switch db
+fi
